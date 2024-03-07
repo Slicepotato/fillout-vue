76d7ac8ca55e33f7ea3cc517b30acead40dd6f84
Michael Johnson <Kraken@MacBook-Pro-21.local>
2024-03-07 15:57:19 -0500

    init commit

--- src/components/MainRoot.vue
+++ src/components/MainRoot.vue
@@ -24,6 +24,7 @@ export default {
   methods: {
     async fetchResponses() {
       const formId = 'cLZojxk94ous';
+      const apiKey = 'sk_prod_TfMbARhdgues5AuIosvvdAC9WsA5kXiZlW8HZPaRDlIbCpSpLsXBeZO7dCVZQwHAY3P4VSBPiiC33poZ1tdUj2ljOzdTCCOSpUZ_3912';
       const filters = [
         {
           id: 'nameId',
@@ -33,16 +34,24 @@ export default {
       ];
 
       try {
-        const response = await axios.get(`https://api.fillout.com/v1/forms/${formId}/filteredResponses`, {
-          params: {
-            filters: JSON.stringify(filters),
-          },
-        });
+        const applyFilters = filters || [];
+        const response = await this.fetchFilteredResponses(formId, apiKey, applyFilters);
 
-        this.responses = response.data.responses;
+        this.responses = response;
       } catch (error) {
-        console.error('Error fetching responses:', error);
+        console.error('Error fetching filtered responses:', error);
       }
+    },
+    async fetchFilteredResponses(formId, apiKey, filters) {
+      const url = `https://api.fillout.com/v1/forms/${formId}/responses`;
+      const headers = {
+        Authorization: `Bearer ${apiKey}`,
+        'Content-Type': 'application/json',
+      };
+
+      const response = await axios.get(url, { headers, params: { filters: JSON.stringify(filters) } });
+
+      return response.data;
     },
   },
 };
